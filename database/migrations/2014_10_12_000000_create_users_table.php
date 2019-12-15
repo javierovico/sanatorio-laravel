b3b638d49e8c4f78ef0e13b17c4618974552b7c7
javierovico <javierovico@gmail.com>
2019-12-15 13:36:59 -0300

    modificacion de la base de datos por defecto para user (agregar cedula, etc)

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,10 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->unsignedInteger('cedula')->unique();
+            $table->text('apellido');
+            $table->string('telefono',20);
+            $table->boolean('habilitado')->default(false);
             $table->rememberToken();
             $table->timestamps();
         });
